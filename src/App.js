450c65d7cd276adbe7e38b4bce5b8ab93cdcce4d
EnesArslan8 <enes8arslan@gmail.com>
2023-07-17 11:24:27 +0300

    User's text handled and add note function created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { useState} from 'react';
 import './App.css';
+import  {AiOutlinePlus}  from "react-icons/ai";
+import Notes from './components/Notes';
+
+
 
 function App() {
+  const [inputText,setInputText]=useState("");
+  const [note,setNote]=useState([])
+
+  
+  const addNote=()=>{
+    setNote([...note,inputText]);
+    setInputText("");
+  }
+
+  console.log(note)
   return (
     <div className="App">
+      <h1 className='title'> Not Alıcı</h1>
+      <div>
+        <input value={inputText} onChange={(e)=>setInputText(e.target.value)}></input>
+        <AiOutlinePlus onClick={addNote}></AiOutlinePlus>
+      </div>
+      {/* <Notes note={note}></Notes> */}
+      
       
     </div>
   );
---
+++ src/components/Notes.js
@@ -0,0 +1,14 @@
+// import React from "react";
+
+// function Notes(note) {
+//     const {not} =note;
+//   return (
+//     <div>
+//         {not && not.map((no, index) => (
+//         <div key={index}>{not}</div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default Notes;
